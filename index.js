1c313ca83df82ffbc15a84a2f7a505e080c2e9bf
Aaron Wilson <adub65@gmail.com>
2020-07-17 14:21:06 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
-// Your code here
+// Remember, all map methods return a new Array.
+
+function mapToNegativize(sourceArray) {
+  let result = []
+  for(const element of sourceArray) {
+    result.push(-element)
+  }
+  return result
+}
+function mapToNoChange(sourceArray) {
+  let result = []
+  for(const element of sourceArray) {
+    result.push(element)
+  }
+  return result
+}
+function mapToDouble(sourceArray) {
+  let result = []
+  for(const element of sourceArray) {
+    result.push(element * 2)
+  }
+  return result
+}
+function mapToSquare(sourceArray) {
+  let result = []
+  for(const element of sourceArray) {
+    result.push(element * element)
+  }
+  return result
+}
+
+// Remember, all reduce methods return a value.
+
+function reduceToTotal(sourceArray, startingPoint = 0) {
+  let result = startingPoint
+  for(const element of sourceArray) {
+    result = result + element
+  }
+  return result
+}
+
+function reduceToAllTrue(sourceArray) {
+  let all_true = true
+  for(const element of sourceArray) {
+    if(!element) { // if any element is false,
+      return false // set all true to false
+    }
+  }
+  return true
+}
+
+function reduceToAnyTrue(sourceArray) {
+  for(const element of sourceArray) {
+    if(element) {
+      return true
+    }
+  }
+  return false
+}
